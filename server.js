1f41ccde1125545bd26fe2f04100427adbfa0422
bennysway <farai.fatso@gmail.com>
2019-11-01 18:04:56 +0100

    Added an extra db

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const snRouter = jsonServer.router('sn.json')
-const enRouter = jsonServer.router('en.json')
+const snRouter = jsonServer.router('sn_zw.json')
+const enRouter = jsonServer.router('en_zw.json')
 const port = process.env.PORT || 8080;
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use('/api/sn',router)
-server.use('/api/sn',router)
+server.use('/api/sn',snRouter)
+server.use('/api/en',enRouter)
 server.listen(port, () => {
   console.log('JSON Server is running')
 })
