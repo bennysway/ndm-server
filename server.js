5de104c3cd8ed7f01a7156240c2af7bee9ac323f
bennysway <farai.fatso@gmail.com>
2019-11-02 09:53:06 +0100

    Cleaned server for database hosting (public and hymns)

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const snRouter = jsonServer.router('sn_zw.json')
-const enRouter = jsonServer.router('en_zw.json')
-const manifestRouter = jsonServer.router('manifest.json')
 const port = process.env.PORT || 8080;
 const middlewares = jsonServer.defaults()
 
+const snRouter = jsonServer.router('sn_zw.json')
+const enRouter = jsonServer.router('en_zw.json')
+const manifestRouter = jsonServer.router('manifest.json')
+const publicRouter = jsonServer.router('public.json')
+
 server.use(middlewares)
+//api
 server.use('/api/sn',snRouter)
 server.use('/api/en',enRouter)
 server.use('/api/manifest',manifestRouter)
+//finally
+server.use(publicRouter)
+//
 server.listen(port, () => {
   console.log('JSON Server is running')
 })
