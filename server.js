13bb514d2837b62af471e17aed306bcb77ee5350
bennysway <farai.fatso@gmail.com>
2020-02-17 06:51:59 +0100

    purge

--- server.js
+++ server.js
@@ -1,88 +1,46 @@
-const jsonServer = require('json-server')
+const express = require('express')
+const firebase = require('firebase')
+const jsRender = require('jsrender')
+var admin = require('firebase-admin')
+const firebaseConfig = require('./fireConfig')
 const fs = require('fs')
-const server = jsonServer.create()
+const app = express()
 const port = process.env.PORT || 8080;
-const middlewares = jsonServer.defaults()
 
-const snRouter = jsonServer.router('sn_zw.json')
-const enRouter = jsonServer.router('en_zw.json')
-const manifestRouter = jsonServer.router('manifest.json')
-const publicRouter = jsonServer.router('public.json')
+app.use('/static', express.static('public'))
 
-server.use(middlewares)
-//time updaters
-server.post('/devotions', (req, res, next) => {
-    readManifestAndUpdate('devotion_ndm_ZW')
-    next()
-});
-server.post('/music', (req, res, next) => {
-    readManifestAndUpdate('music_ndm_ZW')
-    next()
-});
-server.post('/events', (req, res, next) => {
-    readManifestAndUpdate('event_ndm_ZW')
-    next()
-});
-server.post('/announcements', (req, res, next) => {
-    readManifestAndUpdate('announcement_ndm_ZW')
-    next()
-});
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+  });
+var defaultProject = firebase.initializeApp(firebaseConfig)
+var database = firebase.database();
 
-//api
-server.use('/api/sn',snRouter)
-server.use('/api/en',enRouter)
-server.use('/api/manifest',manifestRouter)
-//finally
-server.use(publicRouter)
-//
-
-function readManifestAndUpdate(databaseId){
-    fs.readFile('manifest.json', (err, data) => {
-        if (err) {
-            throw err;
-            console.log("Cannot update lastModified")
-        } else {
-            try {
-                content = JSON.parse(data)
-                updateManifest(databaseId)
-                //console.log(manifestObject)
-            } catch (e) {
-                console.log("Cannot find Database in manifest")
-            }
-        }
+app.get('/readings/:year/:readingsId', (req, res) => {
+    var id = req.params.readingsId
+    var year = req.params.year
+    var readings = database.ref('/readings/' + year + '/' + id).once('value').then(snapshot => {
+        console.log(snapshot.val())
+        res.send(snapshot.val())
     })
-}
-function updateManifest(dbId) {
-    fs.readFile('manifest.json', (err, data) => {
-        if (err) {
-            throw err;
-            console.log("Cannot update lastModified")
-        } else {
-            try {
-                stringData = data.toString()
-                content = JSON.parse(stringData)
-                manifestOldTime = getDatabaseTimeById(content.databases, dbId)
-                console.log(manifestOldTime)
-                stringData = stringData.replace(manifestOldTime.toString(), (new Date()).getTime().toString())
-                fs.writeFile('manifest.json', stringData, err=> {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    console.log("The file was saved!");
-                }); 
-            } catch (e) {
-                console.log("Cannot update Database in manifest")
-                console.log(e)
-            }
-        }
-    })
-}
-function getDatabaseTimeById(db, dbId) {
-    return db.filter(data => {
-        return data.databaseId == dbId
-    })[0].lastModified
-}
+})
 
-server.listen(port, () => {
-  console.log('JSON Server is running')
+app.get('/login', (req, res) => {
+    var tmpl = jsRender.templates('./public/html/signin.html')
+    var signInOptions = [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID
+      ]
+    res.send(tmpl.render({
+        firebaseConfig: firebaseConfig,
+        signInOptions: signInOptions
+    }))
+})
+
+
+app.listen(port, () => {
+    //Init server
+    console.log('NDM Server is running with: ' + defaultProject.name)
 })
