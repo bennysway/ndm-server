9ba5aba109a8ef44236fbe4291731408d96f0f1e
Ben <bcharumbira@lbpro.pl>
2020-02-18 16:07:22 +0100

    after work

--- app/databaseManager.js
+++ app/databaseManager.js
@@ -4,20 +4,25 @@ var router = express.Router()
 const MongoClient = require('mongodb').MongoClient
 const uri = "mongodb+srv://node-server_1:ndmapp@ndm-api-mvqzp.mongodb.net/test?retryWrites=true&w=majority"
 const client = new MongoClient(uri)
+const cacheService = require('../services/cache')
+const ttl = 60 * 60 * 1; 
+const cache = new cacheService(ttl);
 
 
 router.get('/:collectionName', (req, res) => {
     let collectionName = req.params.collectionName
-    client.connect(err => {
-        client.db("ndm").collection(collectionName).find({}).toArray((err, result) => {
-            if (err) {
-                res.send('error')
-                throw err
-            }
-            console.log(result)
-            res.json({ success: true }).status(200)
-        })
-    })
+    const key = "get_" + collectionName;
+    cache.get(key, () => {
+        return MongoClient.connect(uri)
+            .then(db => {
+                var dbo = db.db("ndm");
+                dbo.collection(collectionName).find({}).toArray(function (err, result) {
+                    if (err) throw err;
+                    return result
+                });
+            })
+    }).then(result => console.log('c'+result))
+    
 })
 
 router.get('/:collectionName/:contentId', (req, res) => {
@@ -29,7 +34,7 @@ router.get('/:collectionName/:contentId', (req, res) => {
                 throw err
             }
             console.log(result)
-            res.json({ success: true }).status(200)
+            res.json(result).status(200)
         })
     })
 })
