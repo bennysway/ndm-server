bc5865f1e030b40156ce057f4953e8fa7a88f5bc
Ben <bcharumbira@lbpro.pl>
2020-02-18 13:44:48 +0100

    Added Landing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jsrender = require('jsrender')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 8080
@@ -6,8 +7,14 @@ const port = process.env.PORT || 8080
 const databaseManager = require('./app/databaseManager')
 
 app.use(bodyParser.json())
-app.use('/static', express.static('public'))
-app.use('/db',databaseManager)
+app.use(express.static(__dirname + '/public'))
+
+app.use('/db', databaseManager)
+
+app.get('/', (req, res) => {
+    let tmpl = jsrender.templates('./public/html/index.html')
+    res.send(tmpl.render({}))
+})
 
 app.listen(port, () => {
     //Init server
