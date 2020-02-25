520e59105e136635c0b76c1561340d9d40331c47
Ben <bcharumbira@lbpro.pl>
2020-02-25 15:55:45 +0100

    7c132ef8(tryin to add web socket...success)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const jsrender = require('jsrender')
 const bodyParser = require('body-parser')
 const app = express()
+const expressws = require('express-ws')(app)
 const port = process.env.PORT || 8080
 
 const databaseManager = require('./app/databaseManager')
@@ -10,11 +11,19 @@ app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
 
 app.use('/db', databaseManager)
+app.ws('/ws', function (ws, req) {
+    ws.on('message', (msg)=> {
+        console.log(msg)
+        ws.send(msg)
+    });
+});
 
 app.get('/', (req, res) => {
     let tmpl = jsrender.templates('./public/html/index.html')
     res.send(tmpl.render({}))
 })
+
+
 
 app.listen(port, () => {
     //Init server
